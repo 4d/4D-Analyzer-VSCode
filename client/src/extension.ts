e0a80ee77fd7a09dfeadd4952db428a00c4f8108
F4r3n <guillaume_k@hotmail.fr>
2022-01-29 10:19:50 +0100

    Add language definition

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -7,6 +7,7 @@ import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
 import * as net from 'net'
 import { ChildProcess, spawn } from 'mz/child_process'
+import * as vscode from 'vscode';
 
 import {
 	LanguageClient,
@@ -24,6 +25,8 @@ export function activate(context: ExtensionContext) {
 	const serverModule = context.asAbsolutePath(
 		path.join('server', 'out', 'server.js')
 	);
+
+
 	// The debug options for the server
 	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
 	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
