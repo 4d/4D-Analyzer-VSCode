2d7ef810193ef08ac32262444cf0fc507151ea4d
Guillaume.kotulski <guillaume.kotulski@4d.com>
2022-07-05 12:25:57 +0200

    Add GoToDef

---
+++ editor/src/test/goToDef.test.ts
@@ -0,0 +1,34 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as vscode from 'vscode';
+import * as assert from 'assert';
+import { getDocUri, activate, setTestContent, setContentAtpos } from './helper';
+
+suite('Go To Def', () => {
+	const docUri = getDocUri('LanguageServerProtocol/Project/Sources/Methods/__method_goToDef_1.4dm');
+
+	test('Go To Def', async () => {
+		await testGoToDef(docUri);
+	});
+});
+
+
+
+async function testGoToDef(docUri: vscode.Uri) {
+
+	let pos = new vscode.Position(3,0)
+
+	await activate(docUri);
+	await setContentAtpos("__method_goToDef_1()", pos);
+
+	const definition = (await vscode.commands.executeCommand(
+		'vscode.provideDefinition',
+		docUri,
+		pos
+	)) as vscode.Definition
+	assert(definition)
+	
+} 
