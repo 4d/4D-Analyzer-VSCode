e023f4085c74b8afc8f2eb14039b7d91e60d3e29
Guillaume Kotulski <guillaume.kotulski@4d.com>
2023-06-19 14:08:38 +0200

    fix TU format

--- editor/src/test/format.test.ts
+++ editor/src/test/format.test.ts
@@ -4,7 +4,7 @@ import * as assert from 'assert';
 import { getDocUri, activate } from './helper';
 
 suite('Format', () => {
-	const docUri = getDocUri('LanguageServerProtocol/Project/Sources/Methods/__method_completion.4dm');
+	const docUri = getDocUri('LanguageServerProtocol/Project/Sources/Methods/__method_formatting.4dm');
 
 	test('Format', async () => {
 		await testFormat(docUri);
@@ -20,11 +20,13 @@ async function testFormat(docUri: vscode.Uri) {
 
 	await activate(docUri);
 
+
 	const textEditList = (await vscode.commands.executeCommand(
 		'vscode.executeFormatDocumentProvider',
-		docUri
-	)) as vscode.CompletionList;
-	assert(textEditList.items.length > 0);
+		docUri,
+		{tabSize: 4, insertSpaces: false}
+	)) as vscode.TextEdit[];
+	assert(textEditList.length > 0);
 	
 } 
 
@@ -33,10 +35,11 @@ async function testFormatRange(docUri: vscode.Uri) {
 	await activate(docUri);
 
 	const textEditList = (await vscode.commands.executeCommand(
-		'vscode.executeFormatRangeProvide',
+		'vscode.executeFormatRangeProvider',
 		docUri,
-        new vscode.Range(new vscode.Position(0, 0), new vscode.Position(1, 0))
-	)) as vscode.CompletionList;
-	assert(textEditList.items.length > 0);
+        new vscode.Range(new vscode.Position(0, 0), new vscode.Position(10, 0)),
+		{tabSize: 4, insertSpaces: false}
+	)) as vscode.TextEdit[];
+	assert(textEditList != undefined);
 	
 } 
