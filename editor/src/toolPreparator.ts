c848a1f360919353348388ee075576887f28fde8
F4r3n <guillaume_k@hotmail.fr>
2024-01-09 15:08:01 +0000

    get latest

--- editor/src/toolPreparator.ts
+++ editor/src/toolPreparator.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as os from 'os';
-import { existsSync, mkdirSync } from "fs";
+import { existsSync, mkdirSync, readdirSync  } from "fs";
 import * as child_process from 'child_process';
 import * as path from 'path';
 
@@ -10,21 +10,28 @@ class LabeledVersion {
     public releaseVersion: number;
     public subversion: number;
     public changelist: number;
+    public isRRelease : boolean;
 
     constructor(version: number, releaseVersion: number, subVersion: number, changelist: number) {
         this.version = version;
         this.releaseVersion = releaseVersion;
         this.changelist = changelist;
         this.subversion = subVersion;
+        this.isRRelease = false;
     }
 
     static fromString(inVersion: string): LabeledVersion {
         const obj: LabeledVersion = new LabeledVersion(0, 0, 0, 0);
         const regex = /^([0-9]{2})(R([0-9]))?(\.([0-9]*))?$/;
         const regexArray = regex.exec(inVersion);
-
+        if(regexArray == null)
+            return obj;
         if (regexArray[1]) {
             obj.version = Number(regexArray[1]);
+        }
+
+        if (regexArray[2]) {
+            obj.isRRelease = regexArray[2].includes("R");
         }
 
         if (regexArray[3]) {
@@ -44,7 +51,7 @@ class LabeledVersion {
 
     public toString(withChangelist: boolean): string {
         let result = String(this.version);
-        if (this.releaseVersion > 0) {
+        if (this.isRRelease) {
             result += "R" + this.releaseVersion;
         }
         else {
@@ -105,19 +112,50 @@ export class ToolPreparator {
         });
     }
 
-    private _download(url: string, filePath: string): Promise<string> {
-        async function download(url, filePath): Promise<string> {
+    private _requestChangeList(url: string): Promise<number> {
+        async function download(url: string): Promise<number> {
             const http = await import('http');
             const https = await import('https');
             const proto = !url.charAt(4).localeCompare('s') ? https : http;
+            return new Promise((resolve, reject) => {
+                const request = proto.get(url, response => {
+                    if (response.statusCode === 302 || response.statusCode === 200) {
+                        const regex = /_([0-9]{6})/;
+                        const resultRegex = regex.exec(response.headers.location);
+                        resolve(Number(resultRegex[1]));
+                    }
+                    else if (response.statusCode !== 200) {
+                        reject(false);
+                    }
+                    else {
+                        reject(false);
+                    }
+                    request.end();
+
+                });
+            });
+        }
+        return new Promise((resolve, reject) => {
+            download(url).then((p) => {
+                resolve(p);
+            }).catch(e => {
+                reject(e);
+            });
+        });
+    }
+
+    private _download(inURL: string, filePath: string): Promise<object> {
+        async function download(inURL, filePath): Promise<object> {
+            const http = await import('http');
+            const https = await import('https');
+            const proto = !inURL.charAt(4).localeCompare('s') ? https : http;
 
             return new Promise((resolve, reject) => {
 
-
-                const request = proto.get(url, response => {
+                const request = proto.get(inURL, response => {
                     if (response.statusCode == 302) {
                         download(response.headers.location, filePath).then(r => {
-                            resolve(r);
+                            resolve({ url: r, changelist: 0 });
                         });
                     }
                     else if (response.statusCode === 200) {
@@ -145,7 +183,7 @@ export class ToolPreparator {
                     }
                     else if (response.statusCode !== 200) {
                         fs.unlink(filePath, () => {
-                            reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
+                            reject(new Error(`Failed to get '${inURL}' (${response.statusCode})`));
                         });
                         return;
                     }
@@ -155,7 +193,7 @@ export class ToolPreparator {
             });
         }
         return new Promise((resolve, reject) => {
-            download(url, filePath).then((p) => {
+            download(inURL, filePath).then((p) => {
                 resolve(p);
             }).catch(e => {
                 reject(e);
@@ -227,9 +265,8 @@ export class ToolPreparator {
         });
     }
 
-    private async _findValidCompressExtension(labeledVersion: LabeledVersion): Promise<string> {
+    private async _getURLFromVersion(labeledVersion: LabeledVersion): Promise<string> {
         const url = this._getURLTool4D(labeledVersion);
-        console.log(url);
 
         try {
             const ok = await this._checkDownloadVersionExist(url);
@@ -243,8 +280,43 @@ export class ToolPreparator {
         }
     }
 
+    private _getTool4DAvailableLocaly(inRootFolder: string, labeledVersion: LabeledVersion) : LabeledVersion {
+        const getDirectories = source =>
+        readdirSync(source, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name);
+
+        let localLabelVersion = labeledVersion;
+        if(localLabelVersion.version == 0) {
+            const versions = getDirectories(inRootFolder).sort(
+                (a, b) => a.localeCompare(b, undefined, { numeric: true }));
+            if(versions.length > 0) {
+                localLabelVersion = LabeledVersion.fromString(versions[0]);
+                return this._getTool4DAvailableLocaly(path.join(inRootFolder, versions[0]), localLabelVersion);
+            }
+        }
+
+        if(localLabelVersion.releaseVersion == 0 && localLabelVersion.isRRelease) {
+            const versions = getDirectories(inRootFolder).sort(
+                (a, b) => a.localeCompare(b, undefined, { numeric: true }));
+            if(versions.length > 0) {
+                localLabelVersion = LabeledVersion.fromString(versions[0]);
+                return this._getTool4DAvailableLocaly(path.join(inRootFolder, versions[0]), localLabelVersion);
+            }
+        }
+
+        if(localLabelVersion.changelist == 0) {
+            const versions = getDirectories(inRootFolder).sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
+            if(versions.length > 0) {
+                localLabelVersion.changelist = Number(versions[0]);
+            }
+        }
+        return localLabelVersion;
+    }
+
     private _getTool4DPath(inRootFolder: string, labeledVersion: LabeledVersion): string {
-        return path.join(inRootFolder, labeledVersion.toString(true));
+        const label = this._getTool4DAvailableLocaly(inRootFolder, labeledVersion);
+        return path.join(inRootFolder, label.toString(false), String(label.changelist));
     }
 
     private _getTool4DExe(inRootFolder: string): string {
@@ -267,30 +339,42 @@ export class ToolPreparator {
     * Download and decompress a toodl
     */
     public async prepareTool4D(inPathToStore: string): Promise<string> {
-
-        const labeledVersionWanted: LabeledVersion = this._versionWanted;
-        const labeledVersion: LabeledVersion = labeledVersionWanted;
         const globalStoragePath = inPathToStore;
+        const tool4DMainFolder = path.join(globalStoragePath, "tool4d");
+        const labeledVersionWanted: LabeledVersion = this._versionWanted;
+        let tool4DExecutable = this._getTool4DExe(path.join(this._getTool4DPath(tool4DMainFolder, labeledVersionWanted), "tool4d"));
+        if (existsSync(tool4DExecutable)) {
+            return tool4DExecutable;
+        }
+
+        const labeledVersion: LabeledVersion = labeledVersionWanted;
+        try {
+            const url = await this._getURLFromVersion(labeledVersion);
+            if (labeledVersion.changelist === 0) {
+                labeledVersion.changelist = await this._requestChangeList(url);
+            }
+        } catch (e) {
+            throw new Error(`Tool4D ${labeledVersion.toString(false)} does not exist`);
+        }
 
         if (!existsSync(globalStoragePath)) {
             mkdirSync(globalStoragePath);
         }
-        const tool4DMainFolder = path.join(globalStoragePath, "tool4d");
         if (!existsSync(tool4DMainFolder)) {
             mkdirSync(tool4DMainFolder);
         }
         const tool4D = this._getTool4DPath(tool4DMainFolder, labeledVersion);
         const zipPath = path.join(tool4D, "tool4d.compressed");
-        const tool4DExecutable = this._getTool4DExe(path.join(tool4D, "tool4d"));
+        tool4DExecutable = this._getTool4DExe(path.join(tool4D, "tool4d"));
         if (!existsSync(tool4DExecutable)) {
 
             if (!existsSync(zipPath)) {
 
                 if (!existsSync(tool4D)) {
-                    mkdirSync(tool4D);
+                    mkdirSync(tool4D, {recursive:true});
                 }
                 try {
-                    const url = await this._findValidCompressExtension(labeledVersion);
+                    const url = this._getURLTool4D(labeledVersion);
                     await this._download(url, zipPath);
                 }
                 catch (error) {
@@ -299,19 +383,16 @@ export class ToolPreparator {
             }
 
             if (existsSync(zipPath)) {
-                try
-                {
+                try {
                     await this._decompress(zipPath, tool4D);
                     return tool4DExecutable;
                 }
-                catch(error)
-                {
+                catch (error) {
                     throw new Error("Cannot decompress the tool4D");
                 }
             }
         }
-        else
-        {
+        else {
             return tool4DExecutable;
         }
     }
