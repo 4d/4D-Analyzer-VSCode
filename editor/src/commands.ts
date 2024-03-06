648a3e062e55612d6fbf708528cd8003797743e2
f4r3n <guillaume_k@hotmail.fr>
2024-03-06 21:08:15 +0100

    WIP

--- editor/src/commands.ts
+++ editor/src/commands.ts
@@ -43,36 +43,38 @@ export function filesStatus(ctx: Ctx): Cmd {
         }));
     };
 }
-let id = 0;
-export function checkSyntax(ctx: Ctx): Cmd {
+
+export function checkWorkspaceSyntax(ctx: Ctx): Cmd {
     return async()=> {
-        id++;
+        
         const client = ctx.client;
         const params = client.code2ProtocolConverter.asTextDocumentIdentifier(
             vscode.window.activeTextEditor.document
         );
-        const response = await client.sendRequest(ext.checkSyntax, params);
-        let currentItem : WorkspaceFullDocumentDiagnosticReport;
-        const diagnosticName : string = client.diagnostics.name;
-        console.log("NAME" + diagnosticName);
-        //.
-        const diagnosticCollection = client.diagnostics;
-        diagnosticCollection.clear();
-        const diagnostics : vscode.Diagnostic[] = [];
+        const response = await client.sendRequest(ext.checkWorkspaceSyntax, params);
+        
+
+        let diagnosticCollection = client.diagnostics;
+        
         for(const diagWorkspace of response.items)
         {
-            currentItem = diagWorkspace as WorkspaceFullDocumentDiagnosticReport;
+            const diagnostics : vscode.Diagnostic[] = [];
+            let currentItem = diagWorkspace as WorkspaceFullDocumentDiagnosticReport;
             for(const diag of currentItem.items)
             {
                 const range : vscode.Range = new vscode.Range(diag.range.start.line, diag.range.start.character, diag.range.end.line, diag.range.end.character);
                 const diagnostic = new vscode.Diagnostic(range, diag.message, diag.severity - 1);
                 diagnostics.push(diagnostic);
             }
+            
             diagnosticCollection.set(vscode.Uri.parse(currentItem.uri), diagnostics);
         }
-        
-        ctx.extensionContext.subscriptions.push(diagnosticCollection);
-
+        /*
+        ctx.extensionContext.subscriptions.push(vscode.languages.onDidChangeDiagnostics((event)=>{
+            event.uris.forEach((uri)=> {
+                console.log(uri, client.diagnostics.get(uri))
+            })
+        }));*/
     };
 }
 
--- editor/src/ctx.ts
+++ editor/src/ctx.ts
@@ -1,10 +1,10 @@
 import * as vscode from 'vscode';
 import * as Commands from "./commands";
-import {Config} from "./config";
+import { Config } from "./config";
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	StreamInfo
+    LanguageClient,
+    LanguageClientOptions,
+    StreamInfo
 } from 'vscode-languageclient/node';
 
 import { workspace } from 'vscode';
@@ -12,84 +12,78 @@ import * as child_process from 'child_process';
 import * as net from 'net';
 
 
+import { ProvideDiagnosticSignature, DocumentDiagnosticRequest, DocumentDiagnosticReportKind, DocumentDiagnosticParams } from 'vscode-languageclient'
 export type CommandCallback = {
     call: (ctx: Ctx) => Commands.Cmd;
 };
 
-export class Ctx
-{
-    private _client : LanguageClient;
-    private _extensionContext : vscode.ExtensionContext;
-    private _commands : Record<string, CommandCallback>;
-    private _config : Config;
+export class Ctx {
+    private _client: LanguageClient;
+    private _extensionContext: vscode.ExtensionContext;
+    private _commands: Record<string, CommandCallback>;
+    private _config: Config;
 
-    constructor(ctx : vscode.ExtensionContext) {
+    constructor(ctx: vscode.ExtensionContext) {
         this._client = null;
         this._extensionContext = ctx;
         this._commands = {};
         this._config = null;
     }
 
-    public get extensionContext() : vscode.ExtensionContext
-    {
+    public get extensionContext(): vscode.ExtensionContext {
         return this._extensionContext;
     }
 
-    public get client() : LanguageClient
-    {
+    public get client(): LanguageClient {
         return this._client;
     }
 
-    public set client(inClient : LanguageClient) {
+    public set client(inClient: LanguageClient) {
         this._client = inClient;
     }
 
-    private _getServerPath(isDebug : boolean) : string {
-        let serverPath : string = this._config.serverPath;
+    private _getServerPath(isDebug: boolean): string {
+        let serverPath: string = this._config.serverPath;
 
-        if(process.env.ANALYZER_4D_PATH)
-        {
+        if (process.env.ANALYZER_4D_PATH) {
             serverPath = process.env.ANALYZER_4D_PATH;
         }
-    
-        if(isDebug) {
+
+        if (isDebug) {
             serverPath = '';//debug
         }
 
         return serverPath;
     }
 
-    private _getPort(isDebug : boolean) : number {
+    private _getPort(isDebug: boolean): number {
         let port = 0;
-        if(process.env.ANALYZER_4D_PORT)
-        {
+        if (process.env.ANALYZER_4D_PORT) {
             port = parseInt(process.env.ANALYZER_4D_PORT);
         }
 
-        if(isDebug) {
+        if (isDebug) {
             port = 1800;
         }
 
         return port;
     }
 
-    public start()
-    {
+    public start() {
         this._config = new Config(this._extensionContext);
         this._config.setContext(this);
         this._config.checkSettings();
-        let isDebug : boolean;
+        let isDebug: boolean;
         isDebug = false;
-        if(process.env.ANALYZER_4D_DEBUG)
-        {
+        if (process.env.ANALYZER_4D_DEBUG) {
             isDebug = true;
         }
-    
-        const serverPath : string = this._getServerPath(isDebug);
-        const port : number= this._getPort(isDebug);
+
+        const serverPath: string = this._getServerPath(isDebug);
+        const port: number = this._getPort(isDebug);
 
         console.log("SERVER PATH", serverPath);
-    
+
         // If the extension is launched in debug mode then the debug server options are used
         // Otherwise the run options are used
         const serverOptions = () =>
@@ -111,24 +105,24 @@ export class Ctx
                         server.close();
                     });
                     //server.close()
-                    resolve({ reader: socket, writer: socket, detached : false });
+                    resolve({ reader: socket, writer: socket, detached: false });
                 });
-    
+
                 // Listen on random port
                 server.listen(port, '127.0.0.1', () => {
                     console.log(`Listens on port: ${(server.address() as net.AddressInfo).port}`);
-                    
-                    if(serverPath != '') {
+
+                    if (serverPath != '') {
                         const childProcess = child_process.spawn(serverPath, [
                             '--lsp=' + (server.address() as net.AddressInfo).port,
                         ]);
-    
+
                         childProcess.stderr.on('data', (chunk: Buffer) => {
                             const str = chunk.toString();
                             console.log('4D Language Server:', str);
                             this._client.outputChannel.appendLine(str);
                         });
-    
+
                         childProcess.on('exit', (code, signal) => {
                             this._client.outputChannel.appendLine(
                                 `Language server exited ` + (signal ? `from signal ${signal}` : `with exit code ${code}`)
@@ -137,30 +131,65 @@ export class Ctx
                                 this._client.outputChannel.show();
                             }
                         });
-    
-    
-                        server.on('close', function() {
+
+
+                        server.on('close', function () {
                             console.log("KILL");
                             childProcess.kill();
                         });
-    
+
                         return childProcess;
                     }
-                   
+
                 });
             });
-    
-            // Options to control the language client
-            const clientOptions: LanguageClientOptions = {
-                // Register the server for plain text documents
-                documentSelector: [{ scheme: 'file', language: '4d' }],
-                synchronize: {
-                    // Notify the server about file changes to '.clientrc files contained in the workspace
-                    fileEvents: workspace.createFileSystemWatcher('**/.4DSettings')
+
+        // Options to control the language client
+        const clientOptions: LanguageClientOptions = {
+            // Register the server for plain text documents
+            documentSelector: [{ scheme: 'file', language: '4d' }],
+            synchronize: {
+                // Notify the server about file changes to '.clientrc files contained in the workspace
+                fileEvents: workspace.createFileSystemWatcher('**/.4DSettings')
+            },
+            initializationOptions: this._config.cfg,
+            diagnosticCollectionName: "4d",
+            middleware: {
+                handleDiagnostics: (uri: vscode.Uri, diagnostics: vscode.Diagnostic[], next: any) => {
+                    console.log("handleDiagnostics", diagnostics)
+                    next(uri, diagnostics);
                 },
-                initializationOptions: this._config.cfg,
-                diagnosticCollectionName:"4d"
-            };
+
+                provideDiagnostics: (document, previousResultId, token, next) => {
+
+                    const next2: ProvideDiagnosticSignature = (document, previousResultId, token) => {
+                        const params: DocumentDiagnosticParams = {
+                            identifier: "4d",
+                            textDocument: { uri: this.client.code2ProtocolConverter.asUri(document instanceof vscode.Uri ? document : document.uri) },
+                            previousResultId: previousResultId
+                        };
+                        if (/*this.isDisposed === true ||*/ !this.client.isRunning()) {
+                            return { kind: DocumentDiagnosticReportKind.Full, items: [] };
+                        }
+                        return this.client.sendRequest(DocumentDiagnosticRequest.type, params, token).then(async (result) => {
+                            if (result === undefined || result === null /*|| this.isDisposed*/ || token.isCancellationRequested) {
+                                return { kind: DocumentDiagnosticReportKind.Full, items: [] };
+                            }
+                            if (result.kind === DocumentDiagnosticReportKind.Full) {
+                                return { kind: DocumentDiagnosticReportKind.Full, resultId: result.resultId, items: await this.client.protocol2CodeConverter.asDiagnostics(result.items, token) };
+                            } else {
+                                return { kind: DocumentDiagnosticReportKind.Unchanged, resultId: result.resultId };
+                            }
+                        }, (error) => {
+                            return this.client.handleFailedRequest(DocumentDiagnosticRequest.type, token, error, { kind: DocumentDiagnosticReportKind.Full, items: [] });
+                        });
+                    };
+
+                    return next2(document, previousResultId, token)
+
+                },
+            }
+        };
         // Create the language client and start the client.
         this._client = new LanguageClient(
             '4D-Analyzer',
@@ -172,17 +201,16 @@ export class Ctx
         this._client.start();
     }
 
-    public registerCommands()
-    {
+    public registerCommands() {
         this._commands = {
-            filesStatus:{call:Commands.filesStatus},
-            checkSyntax:{call:Commands.checkSyntax}
-         };
+            filesStatus: { call: Commands.filesStatus },
+            checkWorkspaceSyntax: { call: Commands.checkWorkspaceSyntax }
+        };
 
-         for (const [name, command] of Object.entries(this._commands)) {
+        for (const [name, command] of Object.entries(this._commands)) {
             const fullName = `4d-analyzer.${name}`;
             const callback = command.call(this);
-           
+
             this._extensionContext.subscriptions.push(vscode.commands.registerCommand(fullName, callback));
         }
     }
@@ -191,12 +219,11 @@ export class Ctx
         this._extensionContext.subscriptions.push(d);
     }
 
-    stop() : undefined | Promise<void>
-    {
+    stop(): undefined | Promise<void> {
         if (!this._client) {
             return undefined;
         }
-    
+
         return this._client.stop();
     }
 }
